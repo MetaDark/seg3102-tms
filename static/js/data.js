a428825c2dc2d53fc8258e28e8d31bc331e2d6e4
MetaDark <kurt.bruneau@gmail.com>
2015-12-05 22:32:50 -0500

    Added data component

---
+++ static/js/data.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var data = (function () {
+  function urlencode(obj) {
+    var arr = [];
+    for (var key in obj) {
+      arr.push(
+        encodeURIComponent(key) + '=' +
+        encodeURIComponent(obj[key]));
+    }
+
+    if (arr.length === 0) return '';
+    return '?' + arr.join('&');
+  }
+  
+  function request(method, url, obj) {
+    var promise = new Promise(function(resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.addEventListener('load', function() {
+        var response = this.responseText;
+        if (!response) {
+          resolve();
+        } else {
+          resolve(JSON.parse(this.responseText));
+        }
+      });
+      
+      var data, contentType;
+      if (method !== 'GET') {
+        data = JSON.stringify(obj);
+        contentType = 'application/json';
+      } else {
+        url += urlencode(obj);
+        contentType = 'application/x-www-form-urlencoded';
+      }
+      
+      xhr.open(method, url);
+      xhr.setRequestHeader('Content-Type', contentType);
+      xhr.send(data);
+    });
+    
+    return promise;
+  }
+  
+  return {
+    get: request.bind(null, 'GET'),
+    put: request.bind(null, 'PUT'),
+    post: request.bind(null, 'POST'),
+    delete: request.bind(null, 'DELETE')
+  };
+})();
