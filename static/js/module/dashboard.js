b98394cfa3dc25fffbf40b27df47578187f64881
MetaDark <kurt.bruneau@gmail.com>
2015-12-06 17:54:39 -0500

    Add dummy content to dashboard

--- static/js/module/dashboard.js
+++ static/js/module/dashboard.js
@@ -4,17 +4,23 @@ app.module(function(E, ajax) {
   var module = {};
 
   module.display = function(container) {
-    Promise.all([
-      ajax.get('teams'),
-      ajax.get('projects'),
-      ajax.get('classes')
-    ]).then(function(data) {
-      var teams = data[0];
-      var projects = data[1];
-      var classes = data[2];
-      console.log(teams, projects, classes);
-    }, function() {
-      app.load('login');
+    var dashboardModal = E('div', {
+      className: 'dashboard modal-dialog',
+      parent: container
+    });
+
+    var modalContent = E('div', {
+      className: 'dashboard-content modal-content',
+      parent: dashboardModal,
+    });
+
+    ajax.get('classes').then(function(classes) {
+      classes.forEach(function (clas) {
+        E('div', {
+          textContent: clas.name + ' - ' + clas.instructor_id,
+          parent: modalContent
+        });
+      });
     });
   };
 
